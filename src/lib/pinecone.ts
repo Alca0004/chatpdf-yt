43dee97d05514aaa2c7d37e5c60e1eab73114c79
alca0004 <alca0004@gmail.com>
2023-10-24 14:56:52 -0400

    Setting up pineconedb

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,6 +1,12 @@
-import { PineconeClient } from '@pinecone-database/pinecone';
+import { PineconeClient, Vector } from '@pinecone-database/pinecone';
 import { downloadFromS3 } from './s3-server';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import {
+  Document,
+  RecursiveCharacterTextSplitter,
+} from '@pinecone-database/doc-splitter';
+import { getEmbeddings } from './embeddings';
+import md5 from 'md5';
 
 let pinecone: PineconeClient | null = null;
 
@@ -15,6 +21,13 @@ export const getPineconeClient = async () => {
   return pinecone;
 };
 
+type PDFPage = {
+  pageContent: string;
+  metaData: {
+    loc: { pageNumber: number };
+  };
+};
+
 export async function loadS3IntoPinecone(fileKey: string) {
   //1. Obtain the PDF
   console.log('Downloading s3 into file system');
@@ -23,10 +36,58 @@ export async function loadS3IntoPinecone(fileKey: string) {
     throw new Error('could not download from s3');
   }
   const loader = new PDFLoader(file_name);
-  const pages = loader.load();
-  return pages;
+  const pages = (await loader.load()) as unknown as PDFPage[];
+
+  //2. Split and segment as Pdf
+  const documents = await Promise.all(pages.map(prepareDocument));
+
+  //3. Vectorise and embed individual documents
+  const vectors = await Promise.all(documents.flat().map(embedDocument));
+}
+
+async function embedDocument(doc: Document) {
+  try {
+    const embeddings = await getEmbeddings(doc.pageContent);
+    const hash = md5(doc.pageContent);
+    return {
+      id: hash,
+      values: embeddings,
+      metadata: {
+        text: doc.metadata.text,
+        pageNumner: doc.metadata.pageNumber,
+      },
+    } as Vector;
+  } catch (error) {
+    console.log('error embedding documents', error);
+    throw error;
+  }
+}
+
+export const truncadeStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes));
+};
+
+async function prepareDocument(page: PDFPage) {
+  let { pageContent, metaData } = page;
+  pageContent = pageContent.replace(/\n/g, ' ');
+  // Split the docs
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metaData.loc.pageNumber,
+        text: truncadeStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
 }
 
 // npm install @pinecone-database/pinecone
 // npm install langchain
 // npm install pdf-parse
+// npm install @pinecone-database/doc-splitter
+// npm install md5
+// npm install @types/md5
